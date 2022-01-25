ceef6e86fe79d8e5e666f6c140ab8bdebba8cd46
Umesh Pawar <alphaumesh07@gmail.com>
2022-01-26 00:52:45 +0530

    Clean Solution of Maximum Element problem

--- Day 2/solution.py
+++ Day 2/solution.py
@@ -1,9 +1,27 @@
-items = [0]
-for i in range(int(input())):
-    nums = list(map(int, input().split()))
-    if nums[0] == 1:
-        items.append(max(nums[1], items[-1]))
-    elif nums[0] == 2:
-        items.pop()
-    else:
-        print(items[-1])
+"""Problem on Maximum element"""
+
+if  __name__ == "__main__":
+
+    stack = []
+
+    #Input
+    for i in range(int(input())):
+        q = list(map(int, input().split()))
+
+
+        #It will push the element
+        if q[0] == 1:
+            if stack:
+                stack.append(max(stack[-1], q[1]))
+            else:
+                stack.append(q[1])
+        
+        # It will delete the element10
+        elif q[0] == 2:
+            stack.pop()
+        
+        # It will print the Maximum element in the stack
+        else:
+            print(stack[-1])
+        
+                       
